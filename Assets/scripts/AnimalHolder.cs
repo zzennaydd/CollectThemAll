76f13cae82f1ead37106970784aa28a2dfc61a10
zzenchery <99723552+zzenchery@users.noreply.github.com>
2024-07-20 23:49:36 +0300

    .

--- Assets/scripts/AnimalHolder.cs
+++ Assets/scripts/AnimalHolder.cs
@@ -7,7 +7,7 @@ public class AnimalHolder : MonoBehaviour
     public AnimalScript animal;
     public AnimalData defaultAnimalData;
 
-    // Start is called before the first frame update
+    
     protected virtual void Start()
     {
         animal.SetAnimal(defaultAnimalData);
